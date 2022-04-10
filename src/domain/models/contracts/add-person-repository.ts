9c2c70e0eca53abbed5f589cf202f4ace9d6f3ec
Luiz Calaça <luizcalaca@gmail.com>
2022-04-09 21:34:13 -0300

    refactor: organizing  the interface that will communicate the domain layer with the infrastructure

--- src/domain/models/contracts/add-person-repository.ts
+++ src/domain/models/contracts/add-person-repository.ts
@@ -1,3 +1,7 @@
+import {PersonModel, AddPersonParams} from "@/domain/models/person";
+
+export const ADD_PERSON_REPOSITORY = "ADD_PERSON_REPOSITORY"
+
 export interface IAddPersonRepository {
-    
+    addUserRepository: (data: AddPersonParams) => Promise<PersonModel>;
 }
